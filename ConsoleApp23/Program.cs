fd7afc15a68709fd1f9bb1e3ed82c61487798228
shakedx <kolesnikov_danil123@mail.ru>
2022-05-26 01:41:15 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = {20,22,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
+int evenNumbers = numbers.Where(s => s % 3 == 0).Count();
+Console.WriteLine($"Замен: {evenNumbers}");
+numbers = numbers.Select(ToZero).ToArray();
+
+foreach (var item in numbers)
+{
+    Console.Write(item+" ");
+}
+
+int ToZero(int n)
+{
+    if (n % 3 == 0) n = 0;
+
+    return n;
+
+}
